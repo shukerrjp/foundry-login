27765a99a90dee48bbb459375edd1f0226ab641c
Rafa Shuker <k.z.shuker@gmail.com>
2022-08-22 18:04:31 +1200

    Initial commit - tmp

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple patch to foundry screen for use with felddy dockerfile and container patch urls.
+
+Follows idea from here: https://www.reddit.com/r/FoundryVTT/comments/o93x4y/loging_screen_modding_088/
---
+++ login-screen-patch.sh
@@ -0,0 +1,3 @@
+wget https://gist.githubusercontent.com/Nordiii/40fd8f15454b973e151691ec9db51cb9/raw/login-screen-patch.css -O patchStyle.css
+cat patchStyle.css >> /home/foundry/resources/app/public/css/style.css
+rm patchStyle.css
